5555e3e42340702f0b2c9e503bf5060fe475bbde
GasparCoto <115457959+GasparCoto@users.noreply.github.com>
2023-12-03 19:20:53 -0600

    Inicio
    Inicio

---
+++ Servicio MVC/Servicio MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Servicio_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
